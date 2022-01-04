aca071651b41bef022a207285ec6894f97b8505f
Priyanshu Agrawal <priyanshuagrawal801@gmail.com>
2022-01-04 10:13:04 +0530

    Create Q12_SquaresinN*NChessboard.cpp

---
+++ Goldman Sachs[Day1-5]/Q12_SquaresinN*NChessboard.cpp
@@ -0,0 +1,7 @@
+long long squaresInChessBoard(long long n) {
+        /*
+        Intution -: sum of squares of n natural numbers is (n*(n+1)*(2n+1))/6;
+        */
+        // code here
+        return n*(n+1)*(2*n+1)/6;
+    }
