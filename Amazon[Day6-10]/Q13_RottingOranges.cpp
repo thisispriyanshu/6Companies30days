421479c69df4f2acfe5682e36e36b8eee91d23fe
Priyanshu Agrawal <priyanshuagrawal801@gmail.com>
2022-01-08 14:37:58 +0530

    Create Q13_RottingOranges.cpp

---
+++ Amazon[Day6-10]/Q13_RottingOranges.cpp
@@ -0,0 +1,30 @@
+ int orangesRotting(vector<vector<int>>& grid) {
+        int min = 0, fresh = 0;
+        queue<pair<int, int>> q;
+        for(int i = 0; i < grid.size(); i++) {
+            for(int j = 0; j < grid[0].size(); j++)
+                if(grid[i][j] == 1) fresh++;
+                else if(grid[i][j] == 2) q.push({i, j});
+        }
+        vector<pair<int, int>> dirs = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+        while(!q.empty()) {
+            int n = q.size();
+            bool rotten = false;
+            for(int i = 0; i < n; i++) {
+                auto x = q.front();
+                q.pop();
+                for(auto cur: dirs) {
+                    int i = x.first + cur.first;
+                    int j = x.second + cur.second;
+                    if(i >= 0 && i < grid.size() && j >= 0 && j < grid[0].size() && grid[i][j] == 1) {
+                        grid[i][j] = 2;
+                        q.push({i, j});
+                        fresh--;
+                        rotten = true;
+                    }
+                }
+            }
+            if(rotten) min++;
+        } 
+        return fresh ? -1 : min;
+    }
